696e166e90a6f166952d66224d2777445e78314a
sethkasten <kastens@umich.edu>
2021-12-03 17:19:01 -0500

    various

--- src/context/UpdateProfileContext.ts
+++ src/context/UpdateProfileContext.ts
@@ -2,7 +2,7 @@ import { createContext } from "react";
 import Profile from "../models/Profile";
 export interface UpdateProfileContextModel {
   profile: Profile | null; // null when not logged in
-  updateProfileHandler: (category: string, updateData: string) => void;
+  updateProfileHandler: (category: string, updateData: any) => void;
 }
 const defaultValue: UpdateProfileContextModel = {
   profile: null,
--- src/models/Profile.ts
+++ src/models/Profile.ts
@@ -1,13 +1,37 @@
-// interface Diet {}
+interface Diet {
+  glutengree: boolean;
+  ketogenic: boolean;
+  vegetarian: boolean;
+  lactovegetarian: boolean;
+  ovovegetarian: boolean;
+  pescetarian: boolean;
+  paleo: boolean;
+  primal: boolean;
+  lowFODMAP: boolean;
+  whole30: boolean;
+}
 
-// interface Intolerances {}
+export interface Intolerances {
+  dairy: boolean;
+  egg: boolean;
+  gulten: boolean;
+  grain: boolean;
+  peanut: boolean;
+  seafood: boolean;
+  sesame: boolean;
+  shellfish: boolean;
+  soy: boolean;
+  sulfite: boolean;
+  treeNut: boolean;
+  wheat: boolean;
+}
 
 export default interface Profile {
   _id?: string;
   uid: string;
   pantry: string[];
   equipment: string[];
-  diet: any;
+  diet: Diet;
   intolerances: any;
   favorites: string[];
 }
