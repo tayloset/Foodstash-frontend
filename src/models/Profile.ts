72ec2bf31baff72e52a1b9489c95f502bb448d21
travis <travismasher@gmail.com>
2021-12-03 16:58:24 -0500

    interfaces frontend

--- src/models/Profile.ts
+++ src/models/Profile.ts
@@ -1,6 +1,30 @@
-// interface Diet {}
+export interface Diet {
+  glutengree: boolean;
+  ketogenic: boolean;
+  vegetarian: boolean;
+  lactovegetarian: boolean;
+  ovovegetarian: boolean;
+  pescetarian: boolean;
+  paleo: boolean;
+  primal: boolean;
+  lowFODMAP: boolean;
+  whole30: boolean;
+}
 
-// interface Intolerances {}
+export interface Intolerances {
+  dairy: boolean;
+  egg: boolean;
+  gulten: boolean;
+  grain: boolean;
+  peanut: boolean;
+  seafood: boolean;
+  sesame: boolean;
+  shellfish: boolean;
+  soy: boolean;
+  sulfite: boolean;
+  treeNut: boolean;
+  wheat: boolean;
+}
 
 export default interface Profile {
   _id?: string;
