c65a6571c909d5b45b594a974143c37b571b3fb2
sethkasten <kastens@umich.edu>
2021-12-15 11:35:33 -0500

    uuuuuuu

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,95 +1,86 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import "./Navigation.css";
 import Search from "../assets/Search.png";
 import Pantry from "../assets/Pantry.png";
 import Intolerances from "../assets/Intolerances.png";
 import Equipment from "../assets/Equipment.png";
 import Favorites from "../assets/Favorites.png";
-import { useContext, useEffect, useState } from "react";
+import { useContext } from "react";
 import AuthContext from "../context/AuthContext";
 
 const Navigation = () => {
   const { user } = useContext(AuthContext);
-  const [path, setPath] = useState(window.location.pathname);
-
-  useEffect(() => {
-    setPath(window.location.pathname);
-  }, []);
 
   return (
     <div className="Navigation">
       <div className="navContainer">
-        <div className={`navLinks${path === "/" ? " selectedPath" : ""}`}>
-          <Link to="/">
+        <div className="navLinks">
+          <NavLink to="/search">
             <img src={Search} alt="Search" />
             <p>Search</p>
-          </Link>
+          </NavLink>
         </div>
         {user ? (
           <>
-            <div
-              className={`navLinks${path === "/pantry" ? " selectedPath" : ""}`}
-            >
-              <Link to="/pantry">
+            <div className="navLinks">
+              <NavLink to="/pantry">
                 <img src={Pantry} alt="Pantry" />
                 <p>Pantry</p>
-              </Link>
+              </NavLink>
             </div>
-            <div
-              className={`navLinks${
-                path === "/intolerances" ? " selectedPath" : ""
-              }`}
-            >
-              <Link to="/intolerances">
+            <div className="navLinks">
+              <NavLink to="/intolerances">
                 <img src={Intolerances} alt="Intolerances" />
                 <p>Intolerances</p>
-              </Link>
+              </NavLink>
             </div>
-            <div
-              className={`navLinks${
-                path === "/equipment" ? " selectedPath" : ""
-              }`}
-            >
-              <Link to="/equipment">
+            <div className="navLinks">
+              <NavLink to="/equipment">
                 <img src={Equipment} alt="Equipment" />
                 <p>Equipment</p>
-              </Link>
+              </NavLink>
             </div>
-            <div
-              className={`navLinks${
-                path === "/favorites" ? " selectedPath" : ""
-              }`}
-            >
-              <Link to="/favorites">
+            <div className="navLinks">
+              <NavLink to="/favorites">
                 <img src={Favorites} alt="Favorites" />
                 <p>Favorites</p>
-              </Link>
+              </NavLink>
             </div>
           </>
         ) : (
           <>
-            <div className="notSignedInContainers signInParagraph">
-              <h3>Sign-in to use these options!</h3>
+            <div className="notSignedInContainers navLinks">
+              <div className="pseudoNav signInParagraph">
+                <h3>Sign-in to use these options!</h3>
+              </div>
             </div>
-            <div className="notSignedInContainers">
-              <img className="notSignedIn" src={Pantry} alt="Pantry" />
-              <p>Pantry</p>
+            <div className="notSignedInContainers navLinks">
+              <div className="pseudoNav">
+                <img className="notSignedIn" src={Pantry} alt="Pantry" />
+                <p>Pantry</p>
+              </div>
             </div>
-            <div className="notSignedInContainers">
-              <img
-                className="notSignedIn"
-                src={Intolerances}
-                alt="Intolerances"
-              />
-              <p>Intolerances</p>
+            <div className="notSignedInContainers navLinks">
+              <div className="pseudoNav">
+                <img
+                  className="notSignedIn"
+                  src={Intolerances}
+                  alt="Intolerances"
+                />
+                <p>Intolerances</p>
+              </div>
             </div>
-            <div className="notSignedInContainers">
-              <img className="notSignedIn" src={Equipment} alt="Equipment" />
-              <p>Equipment</p>
+            <div className="notSignedInContainers navLinks">
+              <div className="pseudoNav">
+                <img className="notSignedIn" src={Equipment} alt="Equipment" />
+                <p>Equipment</p>
+              </div>
             </div>
-            <div className="notSignedInContainers">
-              <img className="notSignedIn" src={Favorites} alt="Favorites" />
-              <p>Favorites</p>
+            <div className="notSignedInContainers navLinks">
+              <div className="pseudoNav">
+                <img className="notSignedIn" src={Favorites} alt="Favorites" />
+                <p>Favorites</p>
+              </div>
             </div>
           </>
         )}
